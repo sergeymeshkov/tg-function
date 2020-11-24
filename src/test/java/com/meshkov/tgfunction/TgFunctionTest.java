6ca6b77a87336391911b64b40822ed6e445d096c
Sergey Meshkov <siarhei.miashkou@leverx.com>
2020-11-24 22:03:43 +0300

    Fix text

--- src/test/java/com/meshkov/tgfunction/TgFunctionTest.java
+++ src/test/java/com/meshkov/tgfunction/TgFunctionTest.java
@@ -1,6 +1,5 @@
 package com.meshkov.tgfunction;
 
-import com.google.cloud.functions.Context;
 import org.junit.jupiter.api.Test;
 
 class TgFunctionTest {
@@ -8,35 +7,7 @@ class TgFunctionTest {
 	TgFunction tgFunction = new TgFunction();
 
 	@Test
-	void accept() {
-		final var json = "{\"test\" : \"test\"}";
-		tgFunction.accept(json, createDummyContext());
-	}
-
-	private Context createDummyContext() {
-		return new Context() {
-
-			@Override
-			public String eventId() {
-				return null;
-			}
-
-			@Override
-			public String timestamp() {
-				return null;
-			}
-
-			@Override
-			public String eventType() {
-				return null;
-			}
-
-			@Override
-			public String resource() {
-				return null;
-			}
-
-		};
+	void service() {
 	}
 
 }
