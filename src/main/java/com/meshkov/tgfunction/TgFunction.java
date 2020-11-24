35397b28eb258a1bedcf58ff7f90b3f924d1e8b6
Sergey Meshkov <siarhei.miashkou@leverx.com>
2020-11-24 21:59:38 +0300

    Replace raw function with http function

--- src/main/java/com/meshkov/tgfunction/TgFunction.java
+++ src/main/java/com/meshkov/tgfunction/TgFunction.java
@@ -1,16 +1,29 @@
 package com.meshkov.tgfunction;
 
-import com.google.cloud.functions.Context;
-import com.google.cloud.functions.RawBackgroundFunction;
+import com.google.cloud.functions.HttpFunction;
+import com.google.cloud.functions.HttpRequest;
+import com.google.cloud.functions.HttpResponse;
 import lombok.extern.java.Log;
 
+import java.io.BufferedReader;
+import java.util.stream.Collectors;
+
 @Log
-public class TgFunction implements RawBackgroundFunction {
+public class TgFunction implements HttpFunction {
 
 	@Override
-	public void accept(String json, Context context) {
-		log.info(json);
-		log.info(context.toString());
+	public void service(HttpRequest request, HttpResponse response) throws Exception {
+		log.info("Method : " + request.getMethod());
+		log.info("Path : " + request.getPath());
+		log.info("Uri : " + request.getUri());
+		log.info("Query param : " + request.getQueryParameters());
+		log.info("Headers : " + request.getHeaders());
+		log.info("Content type : " + request.getContentType());
+		try (final var reader = request.getReader()) {
+			final var body = reader.lines().collect(Collectors.joining());
+			log.info("Body : " + body);
+		}
+
 	}
 
 }
